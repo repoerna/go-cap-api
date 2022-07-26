0da3b1fce916628d397b6196e89306ede2817a95
Fitrisitinurjanah <fitrisitinurjanah@gmail.com>
2022-07-26 14:56:39 +0700

    get dan post

---
+++ app/app.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Start() {
+
+	//create mux
+	mux := mux.NewRouter()
+
+	// * defining routes
+	mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	mux.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	mux.HandleFunc("/customers", addCustomer).Methods(http.MethodPost)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	// * starting the server
+	http.ListenAndServe(":8080", mux)
+}
