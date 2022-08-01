69e4464d99f4c9764f415c9f37f1bade27f41dec
Dwidy99 <98dwiyulianto@gmail.com>
2022-08-01 13:14:18 +0700

    add .env

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"capi/app"
 	"capi/logger"
+	"fmt"
 	"log"
+	"os"
 )
 
+func sanityCheck(){
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+	}
+
+	for _, envKey := range envProps {
+		if os.Getenv(envKey) == "" {
+			logger.Fatal(fmt.Sprintf("environtment variabel %s not defined. terminating application..", envKey))
+		}
+	}
+}
+
 func main() {
+	fmt.Println(os.Getenv("Tets"))
 	log.Printf("\nStarting application...\n")
 	logger.Info("Starting application...(Zap log)")
 	
